1f104a01942e6696ad0db64d4f2eea64aaf4e92c
gregsadetsky <lepetitg@gmail.com>
2023-10-07 19:41:46 -0400

    .

--- app/page.tsx
+++ app/page.tsx
@@ -146,7 +146,7 @@ export default function Home() {
         <p>
           I am entirely not affiliated with the authors of the original
           (amazing) talk & paper. I&apos;d love to be their friend but I&apos;m
-          not sure they&quot;d want that (it&quot;s ok).
+          not sure they&apos;d want that (it&apos;s ok).
         </p>
         <p>
           This was made while I was at{" "}
